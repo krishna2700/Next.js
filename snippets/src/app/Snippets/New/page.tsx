896cd4f46ce3c39dd6a481162691c3ba9547ffa5
Krishna Ruparelia <krishna.ruparelia@profound.works>
2024-06-15 15:06:05 +0530

    Form setup with Create page

---
+++ snippets/src/app/Snippets/New/page.tsx
@@ -0,0 +1,32 @@
+export default function Create() {
+  return (
+    <form>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input
+            name="title"
+            id="title"
+            className="border rounded p-2 w-full"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            name="code"
+            id="code"
+            className="border rounded p-2 w-full"
+          />
+        </div>
+        <button className="rounded p-2 bg-blue-200" type="submit">
+          Create
+        </button>
+      </div>
+    </form>
+  );
+}
