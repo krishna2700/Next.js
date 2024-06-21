8b7d2ad43be9c6c1cc8e2747e100c9491e01897f
Krishna Ruparelia <krishna.ruparelia@profound.works>
2024-06-21 15:26:20 +0530

    testing authentication

--- discuss/src/app/page.tsx
+++ discuss/src/app/page.tsx
@@ -1,8 +1,25 @@
+import * as actions from "@/actions/index";
+import { auth } from "@/auth";
 import { Button } from "@nextui-org/react";
-export default function Home() {
+import Profile from "@/components/profile";
+export default async function Home() {
+  const session = await auth();
   return (
     <div>
-      <Button>Click me</Button>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign in</Button>
+      </form>
+      <form action={actions.signOut}>
+        <Button type="submit">Sign out</Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>sign out</div>
+      )}
+
+      <Profile />
     </div>
   );
 }
