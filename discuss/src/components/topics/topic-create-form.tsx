d0ef14f4d8f758fa40a78cb5938571da11d35a65
Krishna Ruparelia <krishna.ruparelia@profound.works>
2024-06-26 17:16:49 +0530

    Create topic page completed in discuss

--- discuss/src/components/topics/topic-create-form.tsx
+++ discuss/src/components/topics/topic-create-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Input,
   Button,
@@ -7,15 +9,19 @@ import {
   PopoverContent,
 } from "@nextui-org/react";
 import * as actions from "@/actions";
+import { useFormState } from "react-dom";
 
 export default function TopicCreateForm() {
+  const [formState, action] = useFormState(actions.createTopic, {
+    errors: {},
+  });
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4  w-80">
             <h3 className="text-lg">Create a Topic</h3>
             <Input
@@ -23,13 +29,22 @@ export default function TopicCreateForm() {
               label="Name"
               placeholder="Name"
               labelPlacement="outside"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(", ")}
             />
             <Textarea
               name="description"
               label="Description"
               placeholder="Description"
               labelPlacement="outside"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(", ")}
             />
+            {formState.errors._form ? (
+              <div className="rounded p-2 bg-red-200 border border-red-400">
+                {formState.errors._form.join(", ")}
+              </div>
+            ) : null}
             <Button type="submit">Submit</Button>
           </div>
         </form>
