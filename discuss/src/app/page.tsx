8a9a2fc2b1867667333ae00a4de23f5f8de477f4
Krishna Ruparelia <krishna.ruparelia@profound.works>
2024-06-26 12:26:41 +0530

    code clean up

--- discuss/src/app/page.tsx
+++ discuss/src/app/page.tsx
@@ -1,25 +1,7 @@
-import * as actions from "@/actions/index";
-import { auth } from "@/auth";
-import { Button } from "@nextui-org/react";
-import Profile from "@/components/profile";
-export default async function Home() {
-  const session = await auth();
+export default function Home() {
   return (
     <div>
-      <form action={actions.signIn}>
-        <Button type="submit">Sign in</Button>
-      </form>
-      <form action={actions.signOut}>
-        <Button type="submit">Sign out</Button>
-      </form>
-
-      {session?.user ? (
-        <div>{JSON.stringify(session.user)}</div>
-      ) : (
-        <div>sign out</div>
-      )}
-
-      <Profile />
+      <h1>Home page</h1>
     </div>
   );
 }
