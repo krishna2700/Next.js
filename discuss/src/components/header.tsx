2c5bfd84f5a03f5a29ce3a04f6366b9edab4f9a6
Krishna Ruparelia <krishna.ruparelia@profound.works>
2024-06-26 12:47:50 +0530

    cacheing using auth

--- discuss/src/components/header.tsx
+++ discuss/src/components/header.tsx
@@ -1,58 +1,16 @@
-import Link from "next/link";
+import { auth } from "@/auth";
 import {
+  Input,
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Input,
-  Button,
-  Avatar,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
 } from "@nextui-org/react";
-import { auth } from "@/auth";
-import * as actions from "@/actions";
+import Link from "next/link";
+import HeaderAuth from "@/components/header-auth";
 
 export default async function Header() {
   const session = await auth();
-
-  let authContent: React.ReactNode;
-  if (session?.user) {
-    authContent = (
-      <Popover placement="left">
-        <PopoverTrigger>
-          <Avatar src={session?.user?.image || ""} />
-        </PopoverTrigger>
-        <PopoverContent>
-          <div className="p-4">
-            <form action={actions.signOut}>
-              <Button type="submit">Sign out</Button>
-            </form>
-          </div>
-        </PopoverContent>
-      </Popover>
-    );
-  } else {
-    authContent = (
-      <>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="secondary" variant="bordered">
-              Sign in
-            </Button>
-          </form>
-        </NavbarItem>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="primary" variant="flat">
-              Sign up
-            </Button>
-          </form>
-        </NavbarItem>
-      </>
-    );
-  }
 
   return (
     <Navbar className="shadow mb-6">
@@ -66,7 +24,9 @@ export default async function Header() {
           <Input />
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent justify="end">{authContent} </NavbarContent>
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
     </Navbar>
   );
 }
