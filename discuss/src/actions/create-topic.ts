ee760f5a57f11a31a01e61b2623818e25491716c
Krishna Ruparelia <krishna.ruparelia@profound.works>
2024-06-26 15:13:52 +0530

    validation with zod

--- discuss/src/actions/create-topic.ts
+++ discuss/src/actions/create-topic.ts
@@ -1,9 +1,23 @@
 "use server";
 
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, { message: "Must be a lowercase without spaces" }),
+  description: z.string().min(10),
+});
+
 export async function createTopic(formData: FormData) {
-  const name = formData.get("name");
-  const description = formData.get("description");
-  console.log(name, description);
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+  }
 
   // TODO: revalidate the homepage
 }
