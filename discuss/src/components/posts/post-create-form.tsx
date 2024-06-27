3cdce8411f25a40a0eaa930c112b881bf2d463b6
Krishna Ruparelia <krishna.ruparelia@profound.works>
2024-06-27 13:52:43 +0530

    Displaying Creation form

---
+++ discuss/src/components/posts/post-create-form.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useFormState } from "react-dom";
+import {
+  Input,
+  Button,
+  Textarea,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import * as actions from "@/actions";
+import FormButton from "@/components/common/form-button";
+export default function PostCreateForm() {
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create a Post</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a Post</h3>
+            <Input
+              name="title"
+              label="Title"
+              placeholder="Title"
+              labelPlacement="outside"
+            />
+            <Input
+              name="content"
+              label="Content"
+              labelPlacement="outside"
+              placeholder="Content"
+            />
+            <FormButton>Create Post</FormButton>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+}
