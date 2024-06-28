747ca88395152e783ba3c7eef01860dd9210393b
Krishna Ruparelia <krishna.ruparelia@profound.works>
2024-06-28 13:35:59 +0530

    minor changes

--- discuss/src/db/queries/posts.ts
+++ discuss/src/db/queries/posts.ts
@@ -7,6 +7,12 @@ export type PostWithData = Post & {
   _count: { comments: number };
 };
 
+// ? Either above method or below method can be used make sure to comment one and keep
+
+// export type PostWithData = Awaited<
+//   ReturnType<typeof fetchPostsByTopicSlug>
+// >[number];
+
 export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
   return db.post.findMany({
     where: { topic: { slug } },
